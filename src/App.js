36b12640e331421da8bbf55db57450b7c07c5079
Chad Hinds <chadrickj8@gmail.com>
2020-06-05 09:08:26 -0400

    Add a new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from './Components/Header'
+import Balance from './Components/Balance'
+import IncomeExpenses from './Components/IncomeExpenses'
+import {AddTransaction} from './Components/AddTransaction'
+import TransactionList from './Components/TransactionList'
+
 import './App.css'
+
 const App = () => {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 };
