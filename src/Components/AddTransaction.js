43721449e1e31888130b5278db7d140231eba846
Chad Hinds <chadrickj8@gmail.com>
2020-06-05 09:07:47 -0400

    add logic to set the amount using hooks

--- src/Components/AddTransaction.js
+++ src/Components/AddTransaction.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 
-const AddTransaction = () => {
+export const AddTransaction = () => {
     const [text, setText] = useState('');
-    const [amount, setAmount] =useState(0)
+    const [amount, setAmount] = useState(0);
 
     return (
         <div>
@@ -10,16 +10,15 @@ const AddTransaction = () => {
             <form >
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" value={text} onChange={(e)=> setText(e.target.value)}
-                    placeholder="Enter text..." />
+                    <input type="text" value={text} onChange={(e) => setText(e.target.value)}
+                        placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount"
                     >Amount <br />
-            (negative - expense, positive - income)</label
-                    >
+            (negative - expense, positive - income)</label>
                     <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)}
-                    placeholder="Enter amount..." />
+                        placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
@@ -27,4 +26,3 @@ const AddTransaction = () => {
     );
 };
 
-export default AddTransaction;
