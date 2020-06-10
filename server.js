2e1b2c146e7664000ada491e8328a49813f85876
Chad Hinds <chadrickj8@gmail.com>
2020-06-10 11:04:22 -0400

    Setup use of  environmental variables

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const transactions = require('./routes/transactions');
 
 
-// Access your global variables from config folder
+// Access your global variables stored in config folder
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} mode
 on port ${PORT}`.yellow.bold));
