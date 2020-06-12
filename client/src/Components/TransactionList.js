6fd0cb0cada2ed5b75ca68c63afcc99f111bb9b6
Chad Hinds <chadrickj8@gmail.com>
2020-06-12 16:40:44 -0400

    Pass in id as kep prop value

--- client/src/Components/TransactionList.js
+++ client/src/Components/TransactionList.js
@@ -8,13 +8,13 @@ export const TransactionList = (props) => {
 
     useEffect(()=>{
       getTransactions();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
     </>
   )
