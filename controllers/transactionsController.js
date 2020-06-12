7ad75a48ca1ef21afa7a4bc88dfa9be34e2c0557
Chad Hinds <chadrickj8@gmail.com>
2020-06-12 14:05:42 -0400

    Set up deleteTransaction route

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -11,7 +11,7 @@ exports.getTransactions = async (req,res, next) =>{
             data: transactions
         });
     }catch(err) {
-        return res.send(500).json({
+        return res.status(500).json({
             success: false,
             error: 'Server Error'
         });
@@ -21,9 +21,15 @@ exports.getTransactions = async (req,res, next) =>{
 // POST new transaction @route '/api/v1/transactions'
 exports.addTransaction = async (req, res, next) => {
     try {
+        const {text, amount} = req.body;
+        const transaction = await Transaction.create(req.body);
 
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        })
     }catch(err) {
-
+        console.log(err)
     }
 }
 
@@ -32,6 +38,18 @@ exports.deleteTransaction = async (req, res, next) => {
     try {
 
     }catch(err) {
+     if(err.name === 'Validation Error') {
+         const messages = Object.values(err.errors).map(val => val.message);
 
+         res.status(400).json({
+             success: false,
+             error: messages
+         })
+     } else {
+         return res.status(500).json({
+             success: false,
+             error: 'Server Error'
+         });
+     }
     }
 }
