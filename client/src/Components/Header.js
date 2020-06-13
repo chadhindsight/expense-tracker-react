13b5c818f0db58cee364f530e18eb2f759532176
Chad Hinds <chadrickj8@gmail.com>
2020-06-13 11:22:53 -0400

    Pass props as an argument to Header

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div>
-            <h1>Expense tracker</h1>
+            <h1>Expense Tracker</h1>
         </div>
     );
 };
